5be7a8b25ed419157ecd4d171dd0f184ede0f090
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2024-10-09 21:15:35 +0700

    create skeleton for image scan

--- lib/app/modules/image_scan/views/image_scan_view.dart
+++ lib/app/modules/image_scan/views/image_scan_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/app/components/appbar/rempah_sis_appbar.dart';
 import 'package:myapp/app/constant/colors.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../controllers/image_scan_controller.dart';
 
@@ -14,88 +15,92 @@ class ImageScanView extends GetView<ImageScanController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: RempahSISAppbar(),
-      body: Container(
-        alignment: Alignment.topCenter,
-        color: RempahSISColor.lightGrey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: MediaQuery.of(context).size.width * 0.8,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                ),
-                child: Obx(
-                  () => controller.imagePath.value.isEmpty
-                      ? Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            const Icon(Icons.image, size: 100),
-                            const Text('Tidak ada gambar yang dipih.'),
-                          ],
+      body: Obx(() => Skeletonizer(
+            enabled: controller.isLoading.value,
+            child: Container(
+              alignment: Alignment.topCenter,
+              color: RempahSISColor.lightGrey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.width * 0.8,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                      ),
+                      child: Obx(
+                        () => controller.imagePath.value.isEmpty
+                            ? Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  const Icon(Icons.image, size: 100),
+                                  const Text('Tidak ada gambar yang dipih.'),
+                                ],
+                              )
+                            : Image.file(
+                                File(controller.imagePath.value),
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                height: MediaQuery.of(context).size.width * 0.8,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ShadowedContainer(
+                    child: Obx(() => Text(
+                          controller.classificationResult.value.isEmpty
+                              ? 'Klik tombol kamera di bawah untuk mulai mengidentifikasi rempah'
+                              : controller.classificationResult.value,
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                  SizedBox(height: 20),
+                  Obx(() => controller.classificationDescription.isNotEmpty
+                      ? ShadowedContainer(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                controller.classificationDescription.value,
+                                style: TextStyle(fontSize: 12),
+                                textAlign: TextAlign.justify,
+                              ),
+                              SizedBox(height: 10),
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  textStyle: TextStyle(
+                                    fontSize: 12,
+                                    color: RempahSISColor.darkBrown,
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Get.toNamed('/rempah-detail', arguments: {
+                                    'id': controller.classificationId.value
+                                  });
+                                },
+                                child: const Text('Lihat detail rempah',
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        color: RempahSISColor.darkBrown,
+                                        fontWeight: FontWeight.bold)),
+                              )
+                            ],
+                          ),
                         )
-                      : Image.file(
-                          File(controller.imagePath.value),
-                          width: MediaQuery.of(context).size.width * 0.8,
-                          height: MediaQuery.of(context).size.width * 0.8,
-                          fit: BoxFit.cover,
-                        ),
-                ),
+                      : SizedBox()),
+                ],
               ),
             ),
-            SizedBox(height: 20),
-            ShadowedContainer(
-              child: Obx(() => Text(
-                    controller.classificationResult.value.isEmpty
-                        ? 'Klik tombol kamera di bawah untuk mulai mengidentifikasi rempah'
-                        : controller.classificationResult.value,
-                    style: TextStyle(fontSize: 16),
-                    textAlign: TextAlign.center,
-                  )),
-            ),
-            SizedBox(height: 20),
-            Obx(() => controller.classificationDescription.isNotEmpty
-                ? ShadowedContainer(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          controller.classificationDescription.value,
-                          style: TextStyle(fontSize: 12),
-                          textAlign: TextAlign.justify,
-                        ),
-                        SizedBox(height: 10),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            padding: EdgeInsets.zero,
-                            textStyle: TextStyle(
-                              fontSize: 12,
-                              color: RempahSISColor.darkBrown,
-                            ),
-                          ),
-                          onPressed: () {
-                            Get.toNamed('/rempah-detail', arguments: {
-                              'id': controller.classificationId.value
-                            });
-                          },
-                          child: const Text('Lihat detail rempah',
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  color: RempahSISColor.darkBrown,
-                                  fontWeight: FontWeight.bold)),
-                        )
-                      ],
-                    ),
-                  )
-                : SizedBox()),
-          ],
-        ),
-      ),
+          )),
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(9999),
